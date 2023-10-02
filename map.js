eac211a7cab2e2a82b7639f77f40d624cf9fa0cd
Nurullah <hsn.nurullah@gmail.com>
2023-10-02 17:49:13 +0600

    map file added

---
+++ map.js
@@ -0,0 +1,14 @@
+//create map
+const fruits = new Map([
+  ["Apple", 500],
+  ["Banana", 300],
+  ["Oranges", 200],
+]);
+// create a Map
+const flower = new Map();
+// Set Map Values
+flower.set("Rose", 400);
+flower.set("Water Lily", 300);
+flower.get("Rose");
+console.log(flower);
+console.log(flower.get("Rose"));
